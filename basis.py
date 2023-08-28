c3622c9787bd514b10db9d481c3d1e2fd72444f3
JasmeetKaur <143268826+jasMEET-7@users.noreply.github.com>
2023-08-28 17:50:43 +0530

    Add files via upload

---
+++ basis.py
@@ -0,0 +1,19 @@
+
+# m = "I believe in waheguru"
+# print(m.lower())
+# n = "I LOVE WAHEGURu "
+# print(n.replace("u","U"))
+# # strip,lower,replace
+
+# f = input("Enter your name:")
+# print(f.lower())
+#
+# h =input("Enter your age :")
+# print(h.replace("age","AGE"))
+
+# h = input("Enter name :")
+# print(h.upper())
+
+
+j = input("Enter your surname:")
+print(j.strip())
